5333239ebddba624e1c7a29a01b80487b8931923
shivam Ahir <11shivam00@gmail.com>
2023-09-23 15:12:41 +0530

    add to cart option added

--- index.js
+++ index.js
@@ -112,7 +112,6 @@ app.get('/api/iphone', async(req, res) => {
     let iphone = await Iphone.find({});
     res.status(200).send(iphone);
 });
-
 app.get('/api/iphone/:item', async(req, res) => {
     try {
       let itemId = req.params.item; 
@@ -136,7 +135,44 @@ app.get('/api/iphone/:item', async(req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   });
+app.post('/api/iphone/:item', async (req, res) => {
+    console.log('clicked')
+    try {
+      let itemId = req.params.item;
+      let num = 0;
   
+      for (let i = 0; i < itemId.length; i++) {
+        if (itemId[i] == '=') {
+          num = itemId[i + 1];
+          break;
+        }
+      }
+      num = num * 1;
+      num--;
+      console.log(num)
+      console.log(log_userEmail)
+      if (log_userEmail == "") {
+        res.status(500).json({ error: 'User not logged in' });
+      } else {
+        let iphone = await Iphone.find({id:num});
+        const newBuy = new Buy({
+          id: iphone[0].id,
+          bool: iphone[0].bool,
+          name: iphone[0].name,
+          size: iphone[0].size,
+          display: iphone[0].display,
+          link: iphone[0].link,
+          userLog: log_userEmail,
+          price: iphone[0].price
+        });
+        newBuy.save();
+        res.status(201).json({ message: 'Successfuly product added to cart' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});  
 
 app.get('/api/ipad', async(req, res) => {
     let ipad = await Ipad.find({});
@@ -165,6 +201,44 @@ app.get('/api/ipad/:item', async(req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   });
+  app.post('/api/ipad/:item', async (req, res) => {
+    console.log('clicked')
+    try {
+      let itemId = req.params.item;
+      let num = 0;
+  
+      for (let i = 0; i < itemId.length; i++) {
+        if (itemId[i] == '=') {
+          num = itemId[i + 1];
+          break;
+        }
+      }
+      num = num * 1;
+      num--;
+      console.log(num)
+      console.log(log_userEmail)
+      if (log_userEmail == "") {
+        res.status(500).json({ error: 'User not logged in' });
+      } else {
+        let ipad = await Ipad.find({id:num});
+        const newBuy = new Buy({
+          id: ipad[0].id,
+          bool: ipad[0].bool,
+          name: ipad[0].name,
+          size: ipad[0].size,
+          display: ipad[0].display,
+          link: ipad[0].link,
+          userLog: log_userEmail,
+          price: ipad[0].price
+        });
+        newBuy.save();
+        res.status(201).json({ message: 'Successfuly product added to cart' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.get('/api/imac', async(req, res) => {
     let macs = await Mac.find({});
@@ -192,7 +266,45 @@ app.get('/api/imac/:item', async(req, res) => {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
+app.post('/api/imac/:item', async (req, res) => {
+    console.log('clicked')
+    try {
+      let itemId = req.params.item;
+      let num = 0;
+  
+      for (let i = 0; i < itemId.length; i++) {
+        if (itemId[i] == '=') {
+          num = itemId[i + 1];
+          break;
+        }
+      }
+      num = num * 1;
+      num--;
+      console.log(num)
+      console.log(log_userEmail)
+      if (log_userEmail == "") {
+        res.status(500).json({ error: 'User not logged in' });
+      } else {
+        let macs = await Mac.find({id:num});
+        const newBuy = new Buy({
+          id: macs[0].id,
+          bool: macs[0].bool,
+          name: macs[0].name,
+          size: macs[0].size,
+          display: macs[0].display,
+          link: macs[0].link,
+          userLog: log_userEmail,
+          price: macs[0].price
+        });
+        newBuy.save();
+        res.status(201).json({ message: 'Successfuly product added to cart' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.get('/api/iwatch', async(req, res) => {
     let iwatch = await Iwatch.find({});
@@ -220,7 +332,46 @@ app.get('/api/iwatch/:item', async(req, res) => {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
+app.post('/api/iwatch/:item', async (req, res) => {
+    console.log('clicked')
+    try {
+      let itemId = req.params.item;
+      let num = 0;
+  
+      for (let i = 0; i < itemId.length; i++) {
+        if (itemId[i] == '=') {
+          num = itemId[i + 1];
+          break;
+        }
+      }
+      num = num * 1;
+      num--;
+      console.log(num)
+      console.log(log_userEmail)
+      if (log_userEmail == "") {
+        res.status(500).json({ error: 'User not logged in' });
+      } else {
+        let iwatch = await Iwatch.find({id:num});
+        const newBuy = new Buy({
+          id: iwatch[0].id,
+          bool: iwatch[0].bool,
+          name: iwatch[0].name,
+          size: iwatch[0].size,
+          display: iwatch[0].display,
+          link: iwatch[0].link,
+          userLog: log_userEmail,
+          price: iwatch[0].price
+        });
+        newBuy.save();
+        res.status(201).json({ message: 'Successfuly product added to cart' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
+  
 app.get('/api/airpords', async(req, res) => {
     let airpords = await Airpords.find({});
     res.status(200).send(airpords);
@@ -247,8 +398,46 @@ app.get('/api/airpod/:item', async(req, res) => {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  });
-
+});
+app.post('/api/airpod/:item', async (req, res) => {
+    console.log('clicked')
+    try {
+      let itemId = req.params.item;
+      let num = 0;
+  
+      for (let i = 0; i < itemId.length; i++) {
+        if (itemId[i] == '=') {
+          num = itemId[i + 1];
+          break;
+        }
+      }
+      num = num * 1;
+      num--;
+      console.log(num)
+      console.log(log_userEmail)
+      if (log_userEmail == "") {
+        res.status(500).json({ error: 'User not logged in' });
+      } else {
+        let airpords = await Airpords.find({ id:num });
+        const newBuy = new Buy({
+          id: airpords[0].id,
+          bool: airpords[0].bool,
+          name: airpords[0].name,
+          size: airpords[0].size,
+          display: airpords[0].display,
+          link: airpords[0].link,
+          userLog: log_userEmail,
+          price: airpords[0].price
+        });
+        newBuy.save();
+        res.status(201).json({ message: 'Successfuly product added to cart' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
+  
 app.post('/api/signup', (req, res) => {
     const { username, email, password } = req.body;
 
@@ -268,8 +457,8 @@ app.post('/api/signup', (req, res) => {
         res.status(400).json({ message: 'Invalid signup data' });
     }
 });
-var log_userEmail;
-var log_userName;
+var log_userEmail="";
+var log_userName="";
 
 app.post('/api/login', async(req, res) => {
     try {
